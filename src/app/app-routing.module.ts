07183ac6ac7bea067962175c8410513cb3bfe714
wesley mbate <wmbate@meliora.tech>
2022-06-08 17:36:32 +0300

    added useHash for to allow for github pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: "enabled"
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
