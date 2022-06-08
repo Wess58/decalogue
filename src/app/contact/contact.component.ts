20ea997fd869486fe202ac651d2c3559b52b813d
wesley mbate <wmbate@meliora.tech>
2022-06-08 17:49:45 +0300

    added fadeIn onInit animation to contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { style, animate, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss','../shared/footer/footer.component.scss']
+  styleUrls: ['./contact.component.scss','../shared/footer/footer.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class ContactComponent implements OnInit {
 
